75a9aa6745cf40d53e941e530a9969b4169e49ca
Mshindi Chayuga <emmanuelmshindi68@gmail.com>
2026-05-31 12:37:46 +0300

    added the render database url that we need for testing on render

--- src/database/db-config.js
+++ src/database/db-config.js
@@ -1,7 +1,11 @@
 import { Sequelize } from "sequelize";
+import dotenv from 'dotenv';
+dotenv.config();
 
-export const sequelize = new Sequelize('linkdrop', 'postgres', 'Yugah5002@', {
-    host: 'localhost',
+//Changing the database connection to use the DATABASE_URL from the .env file
+//reason: will use render to do testing and render provides the DATABASE_URL for connecting to the database
+
+export const sequelize = new Sequelize(process.env.DATABASE_URL, {
     dialect: 'postgres',
     logging: false
 })
