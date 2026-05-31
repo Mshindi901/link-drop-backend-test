3f05d914c4c557cfbeb5a49041fab1a07b9679ee
Mshindi Chayuga <emmanuelmshindi68@gmail.com>
2026-05-31 12:59:34 +0300

    decided to move the render database url to the config file as a string

--- src/database/db-config.js
+++ src/database/db-config.js
@@ -5,7 +5,7 @@ dotenv.config();
 //Changing the database connection to use the DATABASE_URL from the .env file
 //reason: will use render to do testing and render provides the DATABASE_URL for connecting to the database
 
-export const sequelize = new Sequelize(process.env.DATABASE_URL, {
+export const sequelize = new Sequelize('postgresql://link_drop_db_user:X7k911ThYSYz3Ovsh2cBppmvAWEIG70J@dpg-d8e00lrbc2fs73c4ul7g-a/link_drop_db', {
     dialect: 'postgres',
     logging: false
 })
