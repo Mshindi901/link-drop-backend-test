4fddcadfffb1fad09dbae34feec863a23100a2a7
Mshindi Chayuga <emmanuelmshindi68@gmail.com>
2026-06-19 13:43:53 +0300

    configured ssl in db configuration

--- src/database/db-config.js
+++ src/database/db-config.js
@@ -8,5 +8,11 @@ const db_url = process.env.DATABASE_URL
 
 export const sequelize = new Sequelize('postgresql://link_drop_db_user:X7k911ThYSYz3Ovsh2cBppmvAWEIG70J@dpg-d8e00lrbc2fs73c4ul7g-a.frankfurt-postgres.render.com/link_drop_db', {
     dialect: 'postgres',
+    dialectOptions: {
+        ssl: {
+            require: true,
+            rejectUnauthorized: false
+        }
+    },
     logging: false
 })
