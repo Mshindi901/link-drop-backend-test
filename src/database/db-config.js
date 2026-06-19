a294ff567265af1c41ca2889e667ecbb431eae5d
Mshindi Chayuga <emmanuelmshindi68@gmail.com>
2026-06-19 13:30:03 +0300

    update database config

--- src/database/db-config.js
+++ src/database/db-config.js
@@ -1,11 +1,12 @@
 import { Sequelize } from "sequelize";
 import dotenv from 'dotenv';
 dotenv.config();
+const db_url = process.env.DATABASE_URL
 
 //Changing the database connection to use the DATABASE_URL from the .env file
 //reason: will use render to do testing and render provides the DATABASE_URL for connecting to the database
 
-export const sequelize = new Sequelize('postgresql://link_drop_db_user:X7k911ThYSYz3Ovsh2cBppmvAWEIG70J@dpg-d8e00lrbc2fs73c4ul7g-a/link_drop_db', {
+export const sequelize = new Sequelize(db_url, {
     dialect: 'postgres',
     logging: false
 })
