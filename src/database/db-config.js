12c1add642de47aa3729ea37820848dcf56779d1
Mshindi Chayuga <emmanuelmshindi68@gmail.com>
2026-06-19 13:39:00 +0300

    debugged database connection

--- src/database/db-config.js
+++ src/database/db-config.js
@@ -6,7 +6,7 @@ const db_url = process.env.DATABASE_URL
 //Changing the database connection to use the DATABASE_URL from the .env file
 //reason: will use render to do testing and render provides the DATABASE_URL for connecting to the database
 
-export const sequelize = new Sequelize(db_url, {
+export const sequelize = new Sequelize('postgresql://link_drop_db_user:X7k911ThYSYz3Ovsh2cBppmvAWEIG70J@dpg-d8e00lrbc2fs73c4ul7g-a.frankfurt-postgres.render.com/link_drop_db', {
     dialect: 'postgres',
     logging: false
 })
